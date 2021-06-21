55c8c487185b2db8a04c27480241ec5c983cfea1
Hugo Mendonça <hugomendonca9@gmail.com>
2021-06-21 01:19:13 -0300

    conectando com o firebase

---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button: React.FC = ({ children }) => {
+    return (
+        <button>{children}</button>
+    )
+}
+
+export default Button;
---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEI,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGED_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SEND_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
